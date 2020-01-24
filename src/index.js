f8843875dc9e6d4b34c69bc96d96205cf79d2076
Kentiba <kentiba93@gmail.com>
2020-01-24 17:25:56 +0100

    updated the app with redux-persist, created directory reducer and moved the data to it , change directory from class to functional component

--- src/index.js
+++ src/index.js
@@ -7,15 +7,17 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./store/rootReducer";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import persistedRedcuer from "./store/rootReducer";
 
 //create store and connect redux to redux-devtools
 let store;
 if (process.env.NODE_ENV === "production") {
-  store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+  store = createStore(persistedRedcuer, compose(applyMiddleware(thunk)));
 } else {
   store = createStore(
-    rootReducer,
+    persistedRedcuer,
     compose(
       applyMiddleware(thunk, logger),
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
@@ -23,11 +25,15 @@ if (process.env.NODE_ENV === "production") {
     )
   );
 }
+//redux-persist
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,23 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
-export default rootReducer;
+//redux persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart", "directory"] //here we tell the config that cart reducer is the only reducer we want to persist, since user is handaled by firebase
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
